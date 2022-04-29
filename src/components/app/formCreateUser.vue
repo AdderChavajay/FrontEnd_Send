143e49c613dcd1c19890faddc05a8324f3a1c688
adder <adderchavajay7@gmail.com>
2022-04-29 15:05:08 -0600

    component createUser realise and login

--- src/components/app/formCreateUser.vue
+++ src/components/app/formCreateUser.vue
@@ -1,3 +1,30 @@
+<script setup>
+import axios from 'axios'
+import {ref} from "vue" 
+import router from '../../router';
+const name = ref('')
+const lastName = ref('')
+const email = ref('')
+const password = ref('')
+const nameDepartament = ref('')
+const confirmPassword = ref('') 
+const codeWork = ref('')
+
+async function register(){
+    await axios.post('http://localhost:80/api/createUser',{
+        name: name.value,
+        lastName: lastName.value,
+        email: email.value,
+        password: password.value,
+        nameDepartament: nameDepartament.value,
+        password_confirmation: confirmPassword.value,
+        codeWork: codeWork.value,
+
+    });
+    router.replace('/login')
+}
+
+</script>
 <template>
     <div class="ContainerCreateUser">
         <div class="formCreateUser">
@@ -10,28 +37,28 @@
                 <div class="names">
                     <fieldset>
                         <legend>Nombres</legend>
-                        <input type="text" name="email" id="email"
+                        <input type="text" name="email" id="email" v-model="name"
                         placeholder="Escriba los nombres">
                     </fieldset>
                 </div>
                 <div class="lastNames">
                     <fieldset>
                         <legend>Apellidos</legend>
-                        <input type="text" name="email" id="email"
+                        <input type="text" name="email" id="email" v-model="lastName"
                         placeholder="Escriba los apellidos" >
                     </fieldset>
                 </div>
-                <div class="codigWork">
+                <div class="codegWork">
                     <fieldset>
                         <legend>Codigo de Trabajo de Area</legend>
-                        <input type="text" name="email" id="email" 
+                        <input type="text" name="codegWork" id="email" v-model="codeWork"
                         placeholder="Codigo de Trabajo de Area">
                     </fieldset>
                 </div>
                 <div class="nameDepartament">
                     <fieldset>
                         <legend>Nombre del Departamento</legend>
-                        <input type="text" name="nameDepartament" id="nameDepartament" 
+                        <input type="text" name="nameDepartament" id="nameDepartament" v-model="nameDepartament"
                         placeholder="Nombre de Departamento de Area">
                     </fieldset>
                 </div>
@@ -39,27 +66,27 @@
                 <div class="email">
                     <fieldset>
                         <legend>Correo Electronico</legend>
-                        <input type="email" name="email" id="email" 
+                        <input type="email" name="email" id="email" v-model="email"
                         placeholder="Email">
                     </fieldset>
                 </div>
                 <div class="password">
                     <fieldset>
                         <legend>Contraseña</legend>
-                        <input type="password" name="password" id="password" 
+                        <input type="password" name="password" id="password" v-model="password"
                         placeholder="Password">
                     </fieldset>
                 </div>
                 <div class="confirPassword">
                     <fieldset>
                         <legend>Confimar Contraseña</legend>
-                        <input type="password" name="password" id="password" 
+                        <input type="password" name="password" id="password" v-model="confirmPassword"
                         placeholder="Repit your Password">
                     </fieldset>
                 </div>
                 <hr>
                 <div class="btn_aceptar">
-                    <button type="submit">Aceptar</button>
+                    <button @click.prevent="register"  type="submit">Aceptar</button>
                 </div>
             </form>
         </div>
--- src/components/app/formLogin.vue
+++ src/components/app/formLogin.vue
@@ -1,3 +1,20 @@
+<script setup>
+import axios from 'axios'
+import {ref} from "vue" 
+import router from '../../router';
+
+const email = ref('')
+const password = ref('')
+
+async function login(){
+    await axios.post('http://localhost:80/api/login',{
+        email: email.value,
+        password: password.value,
+    })
+    router.replace('/main')
+}
+
+</script>
 <template>
     <section class="login">
         <div class="container">
@@ -12,13 +29,13 @@
                 <div class="email">
                     <fieldset>
                         <legend>Correo Electronico</legend>
-                        <input type="email" name="email" id="email" >
+                        <input v-model="email" type="email" name="email" id="email" >
                     </fieldset>
                 </div>
                 <div class="password">
                     <fieldset>
                         <legend>Contraseña</legend>
-                        <input type="password" name="password" id="password" >
+                        <input type="password" v-model="password" name="password" id="password" >
                     </fieldset>
                 </div>
                 <div class="forgot_pass">
@@ -26,7 +43,7 @@
                 </div>
                     <hr>
                 <div class="btn_aceptar">
-                    <button type="submit">Aceptar</button>
+                    <button @click.prevent="login" type="submit">Aceptar</button>
                 </div>
             </form>
             </div>
